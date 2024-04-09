ffa7b54f09425cfe0a4cb37e2d8dcd11fa77c1d5
jokertitcho <jokertitcho@yahoo.fr>
2024-04-09 15:49:07 +0200

    plus d'idée

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ import PokemonByGeneration from "./componants/PokemonByGeneration";
 import FindPokemon from "./componants/FindPokemon";
 
 function App() {
+  
+const gen1 = 1
+const gen2 = 2
+const gen3 = 3
+const gen4 = 4
+const gen5 = 5
+const gen6 = 6
+const gen7 = 7
+const gen8 = 8
+ 
+
   return (
     <div className="App">
       <Header/>
@@ -15,7 +26,16 @@ function App() {
       <OneHundred/>
       <Types/>
       <FeuEauPlante/> */}
-      <PokemonByGeneration/>
+      <PokemonByGeneration generation={gen1} />
+      {/* <PokemonByGeneration generation={gen2} />
+      <PokemonByGeneration generation={gen3} />
+      <PokemonByGeneration generation={gen4} />
+      <PokemonByGeneration generation={gen5} />
+      <PokemonByGeneration generation={gen6} />
+      <PokemonByGeneration generation={gen7} />
+      <PokemonByGeneration generation={gen8} /> */}
+      
+
       <FindPokemon/>
 
     </div>
--- src/componants/PokemonByGeneration.jsx
+++ src/componants/PokemonByGeneration.jsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from 'react';
 
-const PokemonByGeneration = () => {
-    const [arrGenerationType, setArrGenerationType] = useState()
+const PokemonByGeneration = ({generation}) => {
 
+    console.log(generation);
+     
+    const [arrGenerationType, setArrGenerationType] = useState(null)
+    
     useEffect(()=>{
-        fetch("https://pokebuildapi.fr/api/v1/pokemon/generation/3")
-        .then((data)=>{return data.json()})
-        .then((arrGen)=> setArrGenerationType(arrGen))
-    }, [])
 
+            fetch("https://pokebuildapi.fr/api/v1/pokemon/generation/" + generation)
+            .then((data)=>{return data.json()})
+            .then((arrGen)=> {setArrGenerationType(arrGen)})
+             
+    }, []);
+    console.log(arrGenerationType);
     return (
         <div>
             {arrGenerationType && <div>
                 {arrGenerationType.map((pokgeneration)=>{
                     return (
-                        <section>
+                        <section key={pokgeneration.id} >
                             <h2>{pokgeneration.name}</h2>
                             <img src={pokgeneration.image} alt={pokgeneration.name}/>
-                            {<p>Type : {pokgeneration.apiTypes[0].name}</p>}
+                            {/* {<p>Type : {pokgeneration.apiTypes[0].name}</p>} */}
                             <p>Generation : {pokgeneration.apiGeneration}</p>
                         </section>
                     )
